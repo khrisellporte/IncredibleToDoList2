2294cae023b26b1f107527d4a36ba1bb0d760a20
khrisellporte <130729373+khrisellporte@users.noreply.github.com>
2024-03-01 23:21:54 -0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+ 
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet} from 'react-native';
+import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
+ 
+ 
+function App() {
+   const [tasks, setTasks] =
+   useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+ 
+  return (
+    <SafeAreaView>
+      <ToDoList tasks ={tasks} />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+ 
+export default App;
